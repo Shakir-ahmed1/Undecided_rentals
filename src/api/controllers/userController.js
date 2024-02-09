359ba3fff4d8028fdf85a1afd31e913afb8d62ab
Favour okerri <favourokerri767@gmail.com>
2024-02-09 23:04:20 +0100

    user registration

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -1,7 +1,28 @@
 //Controllers to handel Users.
+const User  = require('../models/userModel');
+const bcrypt = require('bcrypt');
 
 const register = (req, res, next) => {
-  res.json('user created sucessfully');
+  const {firstName, lastName, email, phoneNumber,  password, confirmPassword } = req.body
+  
+  if (password === confirmPassword) {
+    bcrypt.hash(password, 10).then((hash) => {
+        User.create({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            phoneNumber: phoneNumber,
+            password: hash
+        }).then(() => {
+            res.json("User Registration created Sucessfully")
+        }).catch((err => {
+            res.status(400).json({error: err});
+        }));
+    })
+  } else {
+    res.json('password do not match')
+  }
+
 };
 
 module.exports = { register };
---
+++ src/api/models/userModel.js
@@ -0,0 +1,36 @@
+const { mongoose } = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+
+    lastName: {
+        type: String,
+        required: true,
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    phoneNumber: {
+        type: Number,
+        required: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+
+    confimPassowrd: {
+        type: String,
+        require: true,
+    }
+})
+
+module.exports = mongoose.model('User', userSchema);
