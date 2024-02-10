8657de1bb196caf76c318d10396efae2a911064c
Favour okerri <favourokerri767@gmail.com>
2024-02-10 14:39:05 +0100

    merge

--- src/api/middelware/jwt.js
+++
@@ -1,9 +0,0 @@
-const { sign, verify } = require('jsonwebtoken');
-
-const createToken = (user) => {
-    const accessToken = sign({ userId: user.id, userName:user.email }, 
-        'pizzaallthewayfam', {expiresIn: '1h',});
-    return accessToken;
-}
-
-module.exports = { createToken }
