f1a06438116bd63a131ceee8247354a3da882600
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-02-12 21:30:26 +0300

    updating readme moving the function registerErrorHandler to middleware/errorHandler.js updating .gitignore to ignore package-lock.json

--- backend/src/api/controllers/userController.js
+++ backend/src/api/controllers/userController.js
@@ -1,36 +1,6 @@
 const User = require('../models/userModel');
 const { createToken } = require('../middelware/jwt');
-const { response } = require('express');
-
-function registerErrorHandler(e, password, confirmPassword) {
-   const errors = {
-    firstName : '', lastName : '', email : '', phoneNumber : '', password : '', confirmPassword : '',
-  } 
-  
-  // duplicate email validation
-  if (e.code === 11000) {
-      errors.email = 'That email is already taken';
-      return errors;
-  }
- 
-  // input validation
-  if (e.message.includes('Validation failed')) {
-      Object.values(e.errors).forEach(({properties}) => {
-          errors[properties.path] = properties.message;
-      });
-  }
-  // if no input errors on the password check confirmation
-  if (errors.password !== '') {
-    // handling for empty confirm password
-    if (confirmPassword === '') {
-      errors.confirmPassword = 'Please confirm your password'
-    } else if (password !== confirmPassword) {
-      errors.confirmPassword = 'This field must match your password'
-    }
-  }
-  return errors;
-}
-
+const { registerErrorHandler } = require('../middelware/userErrorHandler');
 
 const register = async (req, res) => {
   const {
@@ -43,7 +13,7 @@ const register = async (req, res) => {
       throw Error('confirm password has a problem');
     }
     const user = await User.validate({firstName, lastName, email, phoneNumber, password});    
-    res.status(200).json({ user });
+    res.status(201).json({ user });
   } catch (e) {
     const errors = registerErrorHandler(e, password, confirmPassword);
     res.status(400).json({ errors });
--- backend/src/api/middelware/userErrorHandler.js
+++ backend/src/api/middelware/userErrorHandler.js
@@ -15,4 +15,34 @@ const errorHandler = (err, req, res, next) => {
   return next(err); // Pass the error to the default Express error handler
 };
 
-module.exports = errorHandler;
+function registerErrorHandler(e, password, confirmPassword) {
+  const errors = {
+   firstName : '', lastName : '', email : '', phoneNumber : '', password : '', confirmPassword : '',
+ } 
+ 
+ // duplicate email validation
+ if (e.code === 11000) {
+     errors.email = 'That email is already taken';
+     return errors;
+ }
+
+ // input validation
+ if (e.message.includes('Validation failed')) {
+     Object.values(e.errors).forEach(({properties}) => {
+         errors[properties.path] = properties.message;
+     });
+ }
+ // if no input errors on the password check confirmation
+ if (errors.password !== '') {
+   // handling for empty confirm password
+   if (confirmPassword === '') {
+     errors.confirmPassword = 'Please confirm your password'
+   } else if (password !== confirmPassword) {
+     errors.confirmPassword = 'This field must match your password'
+   }
+ }
+ return errors;
+}
+
+
+module.exports = { errorHandler, registerErrorHandler };
