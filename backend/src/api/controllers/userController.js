794298fe5603b3c2f2f3fc3e3c271043366a8737
Favour okerri <favourokerri767@gmail.com>
2024-02-13 19:31:46 +0100

    eslint fix

--- backend/src/api/controllers/userController.js
+++ backend/src/api/controllers/userController.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/userModel');
 const { createToken } = require('../middelware/jwt');
 const { registerErrorHandler } = require('../middelware/userErrorHandler');
@@ -7,24 +8,26 @@ const register = async (req, res) => {
     firstName, lastName, email, phoneNumber, password, confirmPassword,
   } = req.body;
   try {
-
-    await User.validate({firstName, lastName, email, phoneNumber, password});
+    await User.validate({
+      firstName, lastName, email, phoneNumber, password,
+    });
     if (password && (confirmPassword === '' || confirmPassword !== password)) {
       throw Error('confirm password has a problem');
     }
-    const user = await User.create({firstName, lastName, email, phoneNumber, password});    
+    const user = await User.create({
+      firstName, lastName, email, phoneNumber, password,
+    });
     res.status(201).json({ user });
   } catch (e) {
     const errors = registerErrorHandler(e, password, confirmPassword);
-    for (const key in errors) {
+    Object.keys(errors).forEach((key) => {
       if (errors[key] === '') {
         delete errors[key];
       }
-    }
+    });
     res.status(400).json({ errors });
-}
+  }
 };
-        
 
 // USer login controller
 const login = async (req, res) => {
@@ -61,6 +64,6 @@ const login = async (req, res) => {
 const allUsers = async (req, res) => {
   const users = await User.find();
   res.json(users);
-}
+};
 
 module.exports = { register, login, allUsers };
